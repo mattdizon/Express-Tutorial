79316a6a77dcfd4494bd008e5e9da4a6088e58d5
github username <matthewdizon94@protonmail.com>
2019-05-11 10:06:28 -0400

    added nodemon

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // load express module which returns a function
+// dont forget to use nodemon when running server
 const express = require('express')
 //express return value is object of type express
 const app = express()
@@ -13,6 +14,9 @@ const app = express()
 
 //this whole block is how we define a route
 app.get("/",(req, resp)=> {
-resp.send("Hello World")
+resp.send("Hello World!!!")
+})
+app.get('/api/courses', (req, resp) =>{
+    resp.send([1,2,3])
 })
 app.listen(3000, ()=> console.log("listening on port 3000"))
