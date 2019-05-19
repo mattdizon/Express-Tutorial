88b5c3319f06deb42f2a82b26f38b498fa01f665
github username <matthewdizon94@protonmail.com>
2019-05-19 10:05:37 -0400

    post route complete

--- src/models/subscriber.js
+++ src/models/subscriber.js
@@ -6,7 +6,7 @@ const subscriberSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    subscriberToChannel:{
+    subscribedToChannel:{
         type: String,
         required: true
     },
--- src/routes/subs.js
+++ src/routes/subs.js
@@ -4,16 +4,35 @@ const router = express.Router()
 const Subscriber = require(`../models/subscriber`)
 
 //Get all subs
-router.get(`/`,(req,res) =>{
-    res.send("hello world")
+router.get(`/`, async(req,res) =>{
+    try{
+    // await since async method
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch(err){
+        //set status so user knows the error
+        res.status(500).json({message:err.message})
+    }
+
 })
 //Get one sub
 router.get(`/:id`,(req,res) =>{
     res.send(req.params.id)
 })
 //Create sub
-router.post(`/`,(req,res) =>{
-    req.params.id
+router.post(`/`,async(req,res) =>{
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try{
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    }catch(err){
+        // 400 error since it is a user err
+        res.status(400).json({message:err.message})
+
+    }
 })
 //Update sub
 router.patch(`/:id`,(req,res) =>{
